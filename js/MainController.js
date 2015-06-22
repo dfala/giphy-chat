d342622c56ba9eaeb33061bcd6fddd771c7f3229
Daniel Falabella <dnlfala@gmail.com>
2015-06-22 17:56:06 -0600

    Saving data on firebase

--- js/MainController.js
+++ js/MainController.js
@@ -2,18 +2,28 @@ angular.module('giphyApp')
 
 .controller('MainController', function ($scope, getDataService) {
 
+	var initData = function () {
+		getDataService.getInitialData()
+			.then(function (response) {
+				$scope.messages = response;
+			})
+			.catch(function (err) {
+				throw new Error(err);
+			});
+	}
+	initData();
+
+
 	$scope.queryGiphy = function (query) {
 		if (!query) return console.warn('No query :(');
 
-		getDataService.getData(query)
+		getDataService.getGifs(query)
 			.then(function (response) {
-				console.log('response on controller: ', response);
-				$scope.gifs = response;
+				$scope.gif = response;
 			})
 			.catch(function (err) {
 				throw new Error(err);
 			})
-
 	}
 
 })
--- js/getDataService.js
+++ js/getDataService.js
@@ -1,9 +1,10 @@
 angular.module('giphyApp')
 
-.factory('getDataService', function ($http, $q) {
+.factory('getDataService', function ($http, $q, $firebaseArray) {
 	var service = {};
+	var gifRef = new Firebase('https://gifschat.firebaseio.com/gifs');
 
-	service.getData = function (searchQuery) {
+	service.getGifs = function (searchQuery) {
 		var deferred = $q.defer();
 		// Visit API docs for more info: https://github.com/Giphy/GiphyAPI
 		var url = "https://api.giphy.com/v1/gifs/search?q=";
@@ -13,12 +14,41 @@ angular.module('giphyApp')
 
 		$http.get(url)
 			.success(function (response) {
-				// console.log(data);
-				deferred.resolve(response.data);
+				var randomNum = Math.round(Math.random() * (response.data.length - 1));
+				// console.log(response.data[randomNum]);
+				var gif = response.data[randomNum];
+
+				saveMessage(gif);
+				deferred.resolve(gif);
 			})
 			.error(function (err) {
 				deferred.reject(err);
 			});
+
+		return deferred.promise;
+	}
+
+	var saveMessage = function (gifData) {
+		gifRef.push(gifData);
+	}
+
+
+
+	// get initial data
+	service.getInitialData = function () {
+		var deferred = $q.defer();
+
+		gifRef.on('value', function (snapshot) {
+			console.log('firebase data:', snapshot.val())
+			var data = snapshot.val();
+			var tempArray = [];
+
+			for (var key in data) {
+				tempArray.unshift(data[key]);
+			}
+
+			deferred.resolve(tempArray);
+		})
 
 		return deferred.promise;
 	}
