3ce9125da8ebdf66048a28f0d4555cec0051e631
Daniel Falabella <dnlfala@gmail.com>
2015-06-23 21:22:18 -0600

    Front end changes for up and down voting

--- js/MainController.js
+++ js/MainController.js
@@ -1,11 +1,14 @@
 angular.module('giphyApp')
 
-.controller('MainController', function ($scope, giphyService, $firebaseArray) {
+.controller('MainController', function ($scope, $timeout, giphyService, $firebaseArray) {
 	$scope.messages = [];
 
 	var gifRef = new Firebase('https://gifschat.firebaseio.com/gifs');
 
 	$scope.messages = $firebaseArray(gifRef);
+	$timeout(function () {
+		console.log($scope.messages)
+	}, 1000)
 
 	$scope.queryGiphy = function (query) {
 		if (!query) return console.warn('No query :(');
